5eb168ad41a82371a4c3fd36e62bbe3b3487a318
chrisrycx <chrisrycx@gmail.com>
2021-08-20 12:07:51 -0600

    WxContracts functional except for oracle

--- wxinsureproj/contracts/wxinsure.sol
+++ wxinsureproj/contracts/wxinsure.sol
@@ -14,26 +14,33 @@ import "chainlink/contracts/interfaces/AggregatorV3Interface.sol";
 //import "wxcontract.sol";
 
 contract WxContract {
-    uint total_fund = 0;  //GWEI
+    address oracle;  //This is the address that will provide the measured temperature
     address payable public insured;
     address payable public insurer;
-    uint premium;
     uint coverage;
+    uint temperature;
+    uint public measured_temperature = 0;
 
-    constructor(address payable _insured,address payable _insurer,uint _premium,uint _coverage) payable {
+    constructor(address payable _insured,address payable _insurer,uint _coverage,uint _temperature,address _oracle) payable {
         insured = _insured;
         insurer = _insurer;
-        premium = _premium;
         coverage = _coverage;
+        temperature = _temperature;
+        oracle = _oracle;
     }
 
     function claim() public {
         require((msg.sender==insured)||(msg.sender==insurer),"Not party to insurance contract");
+        require(measured_temperature != 0,"Temperature not yet measured");
 
-        if(msg.sender==insured){
-            msg.sender.transfer(coverage);
+        if(measured_temperature <= temperature){
+            //Insurer gets premium + coverage
+            insurer.transfer(coverage);
+            //self destruct?
         }else{
-            msg.sender.transfer(coverage + premium);
+            //Insured gets funds
+            insured.transfer(coverage);
+            //self destruct?
         }
     }
 
@@ -51,8 +58,15 @@ contract InsuranceProvider {
         uint16 temperature; //Kelvin
     }
     mapping(uint => contractSetting) public proposals;
+    address oracle; //Set this to address deploying provider
+    address payable owner; //Set to address deploying Provider
     event newProposal(uint proposalID, uint16 _temperature, uint _coverage);
     event newContract();
+
+    constructor(){
+        oracle = msg.sender;
+        owner = msg.sender;
+    }
 
     function proposeContract(uint coverage, uint premium, uint16 temperature) public payable {
         require(msg.value >= premium + CONTRACT_FEE);
@@ -68,19 +82,24 @@ contract InsuranceProvider {
     function acceptContract(uint proposalid) public payable {
         //Insurer accepts contract by paying coverage
         require(msg.value >= proposals[proposalid].coverage);
-        uint funds = proposals[proposalid].coverage + proposals[proposalid].premium;
-        WxContract _wxcontract = new WxContract {value:funds}(
+        WxContract _wxcontract = new WxContract {value:proposals[proposalid].coverage}(
             proposals[proposalid].insured,
             msg.sender,
-            proposals[proposalid].premium,
-            proposals[proposalid].coverage
+            proposals[proposalid].coverage,
+            proposals[proposalid].temperature,
+            oracle
             );
         emit newContract();
+
+        //Transfer premium to insurer
+        msg.sender.transfer(proposals[proposalid].premium);
     }
 
-    //function cancelContract(contractid)
-
-    //function withdraw()
-    
+    function withdrawFees() public {
+        //Withdraw all Provider fees
+        require(msg.sender == owner,"Only the owner can withdraw fees");
+        owner.transfer(total_fees);
+        total_fees = 0;
+    }
     
 }
